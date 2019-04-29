b36af5f9af06390c07a93aaa11da8b139d114a73
imenmanai <imen.manai@esprit.tn>
2019-04-29 02:25:32 +0200

    Update ajoutpromo.php

--- views/Back office/pages/imen/ajoutpromo.php
+++ views/Back office/pages/imen/ajoutpromo.php
@@ -3,9 +3,6 @@
 include "../../../../entities/imen/promotion.php";
 include "../../../../core/imen/promotionC.php";
 if ($_POST['idee']!=null and $_POST['num']!=null and $_POST['namee']!=null and $_POST['rem']!=null and $_POST['datee']!=null and $_POST['desc']!=null and $_POST['image']!=null) {
-
-
-	
 $promo1=new promotion($_POST['idee'],$_POST['num'],$_POST['namee'],$_POST['rem'],$_POST['datee'],$_POST['desc'],$_POST['image']);
 echo $promo1->getIdprod();
 $promo1C=new promotionC();
